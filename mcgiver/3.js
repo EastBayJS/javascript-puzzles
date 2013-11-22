f128d0a6475b8939b663ac9ec1a4000bb835da5c
garbagemanonfire <nilesvm@gmail.com>
2013-11-21 21:04:38 -0800

    added js puzzles

---
+++ mcgiver/3.js
@@ -0,0 +1,13 @@
+// problem: why is it logging out 0? it should be 12
+// since 4 * 4 is 16, 2 * 2 is 4 and 16 - 4 is 12
+// fix it to log out 12!
+
+function square(num) {
+  result = num * num
+  return result
+}
+
+result = square(4)
+result2 = square(2)
+var subtracted = result - result2
+console.log(subtracted)
