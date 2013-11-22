873dfd14197f098006a643486b7255adb13eb481
JFingert <joshuafingert@gmail.com>
2013-11-22 09:42:32 -0800

    solves puzzles

---
+++ fingert/2.js
@@ -0,0 +1,15 @@
+// problem: why does it log out out 1000?
+// fix it to log 100, 200, 300, 400, 500 etc
+
+// loops 10 times from 0 - 1000 in increments of 100
+var i = 0
+  waitFor(i, function then() {
+  	for (; i <= 1000; i += 100) {
+    console.log(i)
+  }
+})
+
+// this will run a callback function after waiting milliseconds
+function waitFor(milliseconds, callback) {
+  setTimeout(callback, milliseconds)
+}
